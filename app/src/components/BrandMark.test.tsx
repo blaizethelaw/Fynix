f1b241cbee69630c4582a24672f7c3f9ccfacf17
blaizethelaw <blaizegpt@gmail.com>
2025-08-22 20:55:15 -0400

    chore: configure Vercel output

--- app/src/components/BrandMark.test.tsx
+++ app/src/components/BrandMark.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { describe, it, expect } from 'vitest'
 import * as matchers from '@testing-library/jest-dom/matchers'
